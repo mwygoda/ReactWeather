3ddf3d8a57d4d8222dc55c863822f98c3a78bfe9
mwygoda <mwygoda_94@o2.pl>
2016-11-05 21:16:40 +0100

    finishing & fixing

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -4,7 +4,14 @@ var {Link, IndexLink} = require('react-router');
 var Nav = React.createClass({
   onSearch: function (e){
     e.preventDefault();
-    alert('Not yet wired up!');
+
+    var location = this.refs.szukaj.value;
+    var encodedLocation = encodeURIComponent(location);
+
+    if(location.length > 0){
+      this.refs.szukaj.value ='';
+      window.location.hash ="#/?location=" + encodedLocation;
+    }
   },
   render: function(){
     return(
@@ -28,7 +35,7 @@ var Nav = React.createClass({
           <form onSubmit={this.onSearch}>
             <ul className="menu">
               <li>
-                <input type="search" placeholder ="Search weather by city"/>
+                <input type="search" ref="szukaj" placeholder ="Search weather by city"/>
               </li>
               <li>
                 <input type="submit" className="button" value="Get Weather"/>
