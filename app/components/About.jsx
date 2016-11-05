190de4157160e9f4c92fed98d25c83079f2b75dd
mwygoda <mwygoda_94@o2.pl>
2016-11-05 01:17:09 +0100

    About page getting soosososoooso pretty!

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,4 +1,5 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 // var About = React.createClass({
 //   render: function(){
@@ -10,8 +11,26 @@ var React = require('react');
 
 var About = (props) => {
   return (
-    <div><h3>About</h3>
-    <p>Welcome to about page!</p>
+    <div>
+      <h1 className="text-center">About</h1>
+    <p>This application was created to teach me some React js stuff </p>
+    <p>
+      Here are some of the tools I used:
+    </p>
+    <ul>
+      <li>
+        <a href="https://facebook.github.io/react">React</a> - This was the
+          JavaScript framework used.
+      </li>
+      <li>
+        <a href="http://openweathermap.org">Open Weather Map</a> - I used
+          Open Wether Map to search for weather data by city name.
+      </li>
+      <li>
+        <a href="https://github.com/mwygoda/ReactWeather">application code</a> -
+          whole code available here
+      </li>
+    </ul>
   </div>
 
   )
